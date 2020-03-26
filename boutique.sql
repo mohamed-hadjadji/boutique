daa4f47fd030a900881befd7093565e1761ef580
mohamed-hadjadji <55872437+mohamed-hadjadji@users.noreply.github.com>
2020-03-26 11:41:28 +0100

    mise a jour BDD

--- boutique.sql
+++ boutique.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 23 mars 2020 à 19:59
+-- Généré le :  jeu. 26 mars 2020 à 10:36
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.2.18
 
@@ -27,6 +27,30 @@ USE `boutique`;
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `avis`
+--
+
+DROP TABLE IF EXISTS `avis`;
+CREATE TABLE IF NOT EXISTS `avis` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `commentaire` varchar(255) NOT NULL,
+  `id_utilisateur` int(11) NOT NULL,
+  `id_produit` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `avis`
+--
+
+INSERT INTO `avis` (`id`, `commentaire`, `id_utilisateur`, `id_produit`, `date`) VALUES
+(14, 'Top de des smartphones', 1, 39, '2020-03-25 12:54:10'),
+(13, 'TÃ©lÃ©phone Apple mon prÃ©fÃ©rÃ© ', 1, 38, '2020-03-25 12:47:25');
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `produits`
 --
 
@@ -38,19 +62,20 @@ CREATE TABLE IF NOT EXISTS `produits` (
   `prix` varchar(255) NOT NULL,
   `categorie` varchar(255) NOT NULL,
   `souscategorie` varchar(255) NOT NULL,
+  `quantite` varchar(255) NOT NULL,
   `icon` varchar(255) NOT NULL,
   `id_utilisateur` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=40 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=42 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `produits`
 --
 
-INSERT INTO `produits` (`id`, `titre`, `info`, `prix`, `categorie`, `souscategorie`, `icon`, `id_utilisateur`) VALUES
-(39, 'HUAWEI P30', 'Smartphone HUAWEI P30 lite Bleu 128 Go', '300', 'HUAWEI ', 'smartphone', 'uploads/huawei-p30.jpg', 1),
-(36, 'Galaxy S20', 'Samsung Galaxy S20 128 Go Bleu', '800', 'Samsung ', 'smartphone', 'uploads/samsung-galaxy.jpg', 1),
-(38, 'APPLE iPhone 11', 'APPLE iPhone 11 Noir 64 Go', '900', 'APPLE ', 'smartphone', 'uploads/apple-iphone-11.jpg', 1);
+INSERT INTO `produits` (`id`, `titre`, `info`, `prix`, `categorie`, `souscategorie`, `quantite`, `icon`, `id_utilisateur`) VALUES
+(39, 'HUAWEI P30', 'Smartphone HUAWEI P30 lite Bleu 128 Go', '300', 'HUAWEI ', 'smartphone', '', 'uploads/huawei-p30.jpg', 1),
+(41, 'Galaxy S20', 'Ecran Infinity Dynamic Triple Capteur Photo 64 Mp Space Zoom x30 Photo et vidÃ©os 8K  MÃ©moire RAM 12 Go Stockage 128 Go Port microSD jusquÃ  1 To Batterie 4000 mAh Charge rapide 25W RÃ©seau 4G', '800', 'Samsung ', 'smartphone', '30', 'uploads/samsung-galaxy.jpg', 1),
+(38, 'APPLE iPhone 11', 'APPLE iPhone 11 Noir 64 Go', '900', 'APPLE ', 'smartphone', '', 'uploads/apple-iphone-11.jpg', 1);
 
 -- --------------------------------------------------------
 
