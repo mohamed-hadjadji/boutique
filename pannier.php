46d0f94f52e2242b7ad8181634d25bdf1dddbe1d
Kher <etienne.kher@laplateforme.io>
2020-04-01 13:23:46 +0200

    pannier presque fonctionel

--- pannier.php
+++ pannier.php
@@ -1,7 +1,9 @@
 <?php
    session_start();
    include('fonction.php');
-
+   $pannier=sql("SELECT id_prod,SUM(pannier.quantite),titre,prix,icon FROM `pannier` INNER JOIN `produits` ON id_prod=produits.id WHERE id_user=".$_SESSION['id']." GROUP BY id_prod");
+   
+   var_dump($pannier);
   //include("class.php");
    ?>
 
@@ -20,5 +22,28 @@
             <?php include ('bar-nav.php')?>
 
         </header>
+        <main>
+         <h1>Votre pannier :</h1>
+         <?php
+         $pantotal=0;
+          foreach ($pannier as $p) 
+          {
+            $ptotal=$p[3]*$p[1];
+            $pantotal+=$ptotal;
+          ?>
+          <div>
+            <h><?=$p[2]?></h>
+            <img src="<?=$p[4]?>"/>
+            <p>prix: <?=$p[3]?>€</p>
+            <p>total: <?=$p[3]?> x <?=$p[1]?>= <?=$ptotal?>€</p>
+            <a href="produit.php?id=<?=$p[0]?>" >voir le produit</a>
+
+          </div>
+          <?php       
+          }
+
+         ?> 
+         <h1>total du pannier: <?=$pantotal?></h1>
+        </main>
     </body>
 </html>
