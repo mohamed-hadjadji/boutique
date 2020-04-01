70e26a58f56a017586faeea9b6a4f084dd345165
Kher <etienne.kher@laplateforme.io>
2020-04-01 13:56:25 +0200

    pannier supression article

--- pannier.php
+++ pannier.php
@@ -1,6 +1,10 @@
 <?php
    session_start();
    include('fonction.php');
+   if(isset($_POST['supan']))
+   {
+    sql("DELETE FROM `pannier` WHERE id_user=".$_SESSION['id']." && id_prod=".$_POST['supan']."");
+   }
    $pannier=sql("SELECT id_prod,SUM(pannier.quantite),titre,prix,icon FROM `pannier` INNER JOIN `produits` ON id_prod=produits.id WHERE id_user=".$_SESSION['id']." GROUP BY id_prod");
    
    var_dump($pannier);
@@ -25,9 +29,13 @@
         <main>
          <h1>Votre pannier :</h1>
          <?php
+         if (!empty($pannier)) 
+         {
+           
+         
          $pantotal=0;
           foreach ($pannier as $p) 
-          {
+           {
             $ptotal=$p[3]*$p[1];
             $pantotal+=$ptotal;
           ?>
@@ -37,13 +45,21 @@
             <p>prix: <?=$p[3]?>€</p>
             <p>total: <?=$p[3]?> x <?=$p[1]?>= <?=$ptotal?>€</p>
             <a href="produit.php?id=<?=$p[0]?>" >voir le produit</a>
-
+            <form method="post">
+              <button type="submit" name="supan" value="<?=$p[0]?>">suprimer</button>
+            </form>
           </div>
           <?php       
+           }
+           ?><h1>total du pannier: <?=$pantotal?>€</h1><?php
           }
-
+          else
+          {?>
+            <p>votre pannier est vide</p>
+          <?php }
          ?> 
-         <h1>total du pannier: <?=$pantotal?></h1>
+         
+         <!-- simulation de paiment!-->
         </main>
     </body>
 </html>
