9e8e37f7167707412cf917bd492ed9fa2c53188c
mohamed-hadjadji <55872437+mohamed-hadjadji@users.noreply.github.com>
2020-04-12 14:31:16 +0200

    style connexion inscription profil

--- inscription.php
+++ inscription.php
@@ -12,10 +12,10 @@
 <head>
     <meta charset="UTF-8">
     <title> Inscription</title>
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="style.css">
 </head>
 
-<body class="">
+<body class="bodiprof">
     <header>
 
     <?php 
@@ -26,60 +26,61 @@
     ?>
 
     </header>
-    
-        <section class="conteneur">
-        <h1> Inscription </h1>
+      <section class="proform">
+        <article id="cadins">
+          <h1> Inscription </h1>
 
-        <form method='POST' action=''>
-           
-                <label>Login</label>
-                <input type="text" name='login' required />
-                       
-                <label>Prenom</label>
-                <input type="text" name='prenom' required />
-                      
-                <label>Nom</label>
-                <input type="text" name='nom' required />
+          <form class="form" method='POST' action=''>
+             
+                  <label>Login</label>
+                  <input class="inputa" type="text" name='login' required />
+                         
+                  <label>Prenom</label>
+                  <input class="inputa" type="text" name='prenom' required />
                         
-                <label>Password</label>
-                <input type="password" name='mdp1' required />
-            
-                <label>Confirmation Password</label>
-                <input type="password" name='mdp2' required />
-            
-                <label>Adresse</label>
-                <input type="text" name='adresse' required />
+                  <label>Nom</label>
+                  <input class="inputa" type="text" name='nom' required />
+                          
+                  <label>Password</label>
+                  <input class="inputa" type="password" name='mdp1' required />
+              
+                  <label>Confirmation Password</label>
+                  <input class="inputa" type="password" name='mdp2' required />
+              
+                  <label>Adresse</label>
+                  <input class="inputa" type="text" name='adresse' required />
 
-                <label>Email</label>
-                <input type="text" name='mail'/>
+                  <label>Email</label>
+                  <input class="inputa" type="text" name='mail'/>
 
-                <label>Téléphone</label>
-                <input type="text" name='tele'/>
-          
-            <input class="bouton" type='submit' name='valider' value='Inscription' />
+                  <label>Téléphone</label>
+                  <input class="inputa" type="text" name='tele'/>
+                <div class="valprof">
+                  <input type='submit' name='valider' value='Inscription' />
+                </div>
+             <?php
 
-           <?php
+          if (isset($_POST['valider']))
+         {
+             $register = new user();
 
-        if (isset($_POST['valider']))
-       {
-           $register = new user();
+             $login = $_POST['login'];
+             $mdp= password_hash($_POST["mdp1"], PASSWORD_DEFAULT, array('cost' => 12));
+             $prenom = $_POST['prenom'];
+             $nom =$_POST['nom'];
+             $adresse=$_POST['adresse'];
+             $email=$_POST['mail'];
+             $tele=$_POST['tele'];
 
-           $login = $_POST['login'];
-           $mdp= password_hash($_POST["mdp1"], PASSWORD_DEFAULT, array('cost' => 12));
-           $prenom = $_POST['prenom'];
-           $nom =$_POST['nom'];
-           $adresse=$_POST['adresse'];
-           $email=$_POST['mail'];
-           $tele=$_POST['tele'];
+             $register->register($login,$prenom,$nom,$mdp,$adresse,$email,$tele); 
+              
+             
+          }
 
-           $register->register($login,$prenom,$nom,$mdp,$adresse,$email,$tele); 
-            
-           
-        }
+      ?> 
 
-    ?> 
-
-        </form>
+          </form>
+        </article>
      </section>
     <?php
     }
